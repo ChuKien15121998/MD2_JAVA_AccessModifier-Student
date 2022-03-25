afc3534d0354f113f423e5a4f4ef4eb92a8c9ab4
ChuKien15121998 <chuvankien151298@gmail.com>
2022-03-25 11:03:56 +0700

    Done

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Student student = new Student();
+        student.setName("Kien");
+        student.setClasses("C0222I1");
+        System.out.println(student.toString());
+    }
+}
